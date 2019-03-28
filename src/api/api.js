4c78cec374805a46c62d3cd1f9f17b7cfa4cfd2e
安牛 <zhenhua.xu@kongapi.com>
2019-03-28 10:39:33 +0800

    a

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 // 正式环境
-baseUrl = "www.baidu.com/zhengshi"
+// baseUrl = "www.baidu.com/zhengshi"
 // 测试环境
 // baseUrl = "www.baidu.com/ceshi"
 // 本地环境
-// baseUrl = "www.baidu.com/bendi"
+baseUrl = "www.baidu.com/bendi"
